61fd3ea349512032736f9242220de9a3b27c19b0
user <user@DESKTOP-7PIJGO9>
2025-01-26 19:40:59 +0330

    Add project files.

---
+++ LinqUser/Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinqUser.Models
+{
+    public class DataBaseContext:IdentityDbContext<IdentityUser>
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> Options):base (Options)
+        {
+
+        }
+    }
+}
