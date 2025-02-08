0a79feb91855d40ce0bbeb757786c4dcebb929f9
user <user@DESKTOP-7PIJGO9>
2025-02-08 07:44:16 +0330

    addChange

--- LinqUser/Services/Register/UserRegistrationService.cs
+++ LinqUser/Services/Register/UserRegistrationService.cs
@@ -6,15 +6,18 @@ namespace LinqUser.Services.Register
     public class UserRegistrationService : IUserRegistrationService
     {
         private readonly UserManager<User> _userManager;
-        public UserRegistrationService(UserManager<User> userManager)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserRegistrationService(UserManager<User> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _userManager=userManager;
+            _httpContextAccessor=httpContextAccessor;
         }
-
-        public Task<string> GenerateUniqueSlug(string FirstName)
+        //ساخت Url برای هر کاربر
+        public async Task<string> GenerateUniqueSlug(string FirstName)
         {
+            string baseUrl = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}/profiles/UserProfile/";
             string slug=FirstName .ToLower().Replace(" ","_")+ "-"+Guid.NewGuid().ToString("N").Substring(0,8);
-            return Task.FromResult(slug); ;
+            return baseUrl+ slug;
         }
 
         public async Task<IdentityResult> RegisterUserAsync(RegisterDto model)
